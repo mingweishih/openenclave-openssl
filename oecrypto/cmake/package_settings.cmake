aa7b451fd0b9141c66d4e99764706ba45e21696f
Peiyu Liao <pyliao@stanford.edu>
2019-08-15 23:28:57 +0000

    Update version & create pkg-config files

--- oecrypto/cmake/package_settings.cmake
+++ oecrypto/cmake/package_settings.cmake
@@ -8,6 +8,8 @@ find_package(PkgConfig)
 
 pkg_check_modules(OEOpenSSL openssl_oe_enclave)
 
+# Generate CMake config files
+
 configure_package_config_file(${PROJECT_SOURCE_DIR}/cmake/OECryptoConfig.cmake.in
     ${CMAKE_CURRENT_BINARY_DIR}/OECryptoConfig.cmake
     INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/oecrypto/cmake
@@ -16,11 +18,25 @@ write_basic_package_version_file(
     ${CMAKE_CURRENT_BINARY_DIR}/OECryptoConfigVersion.cmake
     COMPATIBILITY SameMajorVersion)
 
+# Generate pkg-config files
+
+set(DEST_DIR "${CMAKE_INSTALL_PREFIX}")
+set(PRIVATE_LIBS "-L/opt/oe-openssl/lib -lssl -lcrypto")
+
+CONFIGURE_FILE("cmake/oecrypto.pc.in" "oecrypto.pc" @ONLY)
+
+# Install
+
 install(FILES
             ${CMAKE_CURRENT_BINARY_DIR}/OECryptoConfig.cmake
             ${CMAKE_CURRENT_BINARY_DIR}/OECryptoConfigVersion.cmake
-            ${CMAKE_CURRENT_SOURCE_DIR}/cmake/oecryptorc
         DESTINATION ${CMAKE_INSTALL_LIBDIR}/oecrypto/cmake)
+install(FILES
+            ${CMAKE_CURRENT_SOURCE_DIR}/cmake/oecryptorc
+        DESTINATION ${CMAKE_INSTALL_DATADIR}/oecrypto)
+install(FILES
+            ${CMAKE_CURRENT_BINARY_DIR}/oecrypto.pc
+        DESTINATION ${CMAKE_INSTALL_DATADIR}/pkgconfig)
 install(EXPORT OECryptoTargets
         NAMESPACE oecrypto::
         DESTINATION ${CMAKE_INSTALL_LIBDIR}/oecrypto/cmake)
