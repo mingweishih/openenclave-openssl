8bc5069daaec92adc41c2303083f2028f940811c
Radhika Jandhyala <Radhikaj@microsoft.com>
2020-02-26 00:09:02 +0000

    Update copyright headers to include Open Enclave SDK contributors. Update project to use DCO instead of CLA

--- test/enc/get_testcase_name.cmake
+++ test/enc/get_testcase_name.cmake
@@ -1,5 +1,5 @@
 
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Open Enclave SDK contributors.
 # Licensed under the MIT License.
 #
 # Helper function to obtain name for a test-case. Given a filename, chop
